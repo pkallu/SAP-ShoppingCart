aab410fe286a8176186238532f60e99dc647a84f
Pragathi Kallu <pkallu@univ-wea.com>
2017-09-24 17:52:22 -0500

    Added Shopping cart view Controller

--- Shop/DetailViewController.swift
+++ Shop/DetailViewController.swift
@@ -75,6 +75,24 @@ extension DetailViewController: ProductDetailViewDelegate {
     
     func didSelectAddToCart(_ button: FUIButton) {
         
+        guard let product = product else {
+            return
+        }
+        
+        button.isEnabled = false
+        
+        Shop.shared.oDataService?.addProductToShoppingCart(productID: product.id) { shoppingCartItem, error in
+            
+            button.isEnabled = true
+            
+            guard error == nil else {
+                self.logger.warn("Error adding product \(product.name) (\(product.id)) to shopping cart.", error: error)
+                return
+            }
+            
+            FUIToastMessage.show(message: "\(product.name) added to cart.", maxNumberOfLines: 2)
+            NotificationCenter.default.post(name: Shop.shoppingCartDidUpdateNotification, object: nil)
+        }
     }
     
     func didSelectReview(_ review: Review) {
--- Shop/ProductListViewController.swift
+++ Shop/ProductListViewController.swift
@@ -138,5 +138,36 @@ extension ProductListViewController: UITableViewDelegate, UITableViewDataSource 
         }
         return objectCell
     }
-
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let addToCart = UITableViewRowAction(style: .normal, title: "Add to Cart") { _, _ in
+            
+            // remove swiped-in button
+            tableView.setEditing(false, animated: true)
+            
+            let swipedProduct = self.products[indexPath.row]
+            
+            self.logger.debug("Adding product \(swipedProduct.name) (\(swipedProduct.id)) to shopping cart.")
+            Shop.shared.oDataService?.addProductToShoppingCart(productID: swipedProduct.id) { shoppingCartItem, error in
+                
+                guard error == nil else {
+                    self.logger.warn("Error adding product \(swipedProduct.name) (\(swipedProduct.id)) to shopping cart.", error: error)
+                    return
+                }
+                FUIToastMessage.show(message: "\(swipedProduct.name) added to cart.", maxNumberOfLines:2)
+                
+                NotificationCenter.default.post(name: Shop.shoppingCartDidUpdateNotification, object: nil)
+                
+            }
+        }
+        // set color of swiped-in button
+        addToCart.backgroundColor = .preferredFioriColor(forStyle: .tintColorDark)
+        return [addToCart]
+        
+        
+    }
 }
